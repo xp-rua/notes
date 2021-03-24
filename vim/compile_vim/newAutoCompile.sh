853537a50fabeabd44ffba3ac43da2dd9251ee99
zongou <zongou@outlook.com>
2021-03-24 15:32:13 +0800

    update

--- vim/compile_vim/newAutoCompile.sh
+++ vim/compile_vim/newAutoCompile.sh
@@ -11,18 +11,8 @@ cd ~/vim
   --enable-cscope 
 make -j8
 
-checkinstall -y
-
 #apt remove -y vim vim-runtime gvim
 #apt remove -y vim-tiny vim-common vim-gui-common vim-nox
 #rm -rf /usr/share/vim
 # install
-dpkg -i --force-overwrite vim*.deb
-
-# Make vim default editor
-editor=$(which vim)
-	update-alternatives --install /usr/bin/editor editor $editor 1
-	update-alternatives --set editor $editor
-	update-alternatives --install /usr/bin/vi vi $editor 1
-	update-alternatives --set vi $editor
 
