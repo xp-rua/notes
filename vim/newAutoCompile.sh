0afa9ecbf0ed27856e8dc19fbeb05daf719df117
zongou <zongou@outlook.com>
2021-03-24 14:06:56 +0800

    update

---
+++ vim/newAutoCompile.sh
@@ -0,0 +1,20 @@
+
+apt install -y git make libncurses5-dev libncurses5 checkinstall
+
+cd ~/
+git clone https://gitee.com/zongou/vim
+
+cd ~/vim
+./configure --with-features=huge \
+  --enable-multibyte \
+  --enable-gui=false \
+  --enable-cscope 
+make -j8
+
+checkinstall -y
+
+apt remove -y vim vim-runtime gvim
+apt remove -y vim-tiny vim-common vim-gui-common vim-nox
+rm -rf /usr/share/vim
+# install
+#dpkg -i --force-overwrite vim*.deb
