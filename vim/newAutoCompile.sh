0bf240c069742954815856fd30cae73068139c34
zongou <zongou@outlook.com>
2021-03-24 14:17:45 +0800

    update

--- vim/newAutoCompile.sh
+++ vim/newAutoCompile.sh
@@ -13,8 +13,16 @@ make -j8
 
 checkinstall -y
 
-apt remove -y vim vim-runtime gvim
-apt remove -y vim-tiny vim-common vim-gui-common vim-nox
-rm -rf /usr/share/vim
+#apt remove -y vim vim-runtime gvim
+#apt remove -y vim-tiny vim-common vim-gui-common vim-nox
+#rm -rf /usr/share/vim
 # install
-#dpkg -i --force-overwrite vim*.deb
+dpkg -i --force-overwrite vim*.deb
+
+# Make vim default editor
+editor=$(which vim)
+	update-alternatives --install /usr/bin/editor editor $editor 1
+	update-alternatives --set editor $editor
+	update-alternatives --install /usr/bin/vi vi $editor 1
+	update-alternatives --set vi $editor
+
